8620f7e3eafb8e47a8427909b84d1e994eb1760a
Ezlan <eizs798@protonmail.com>
2020-08-15 15:26:48 +0800

    Feat | push whatever has been done

---
+++ resources/js/components/notes/notes-content.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="content">
+        <p class="date">{{ formatedDate }}</p>
+        <textarea class="contentbox" v-if="note" v-model="note.content"></textarea>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        note: {
+            type: Object,
+            required: false
+        }
+    },
+
+    computed: {
+        formatedDate() {
+            let date = null;
+
+            if(this.note) {
+                date = new Date(this.note.updated_at);
+            } else {
+                date = new Date();
+            }
+
+            return date.toLocaleString('en-gb', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit'
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    
+</style>
